f915468f0f0a16a26e44b56f7d6eb51673d83f4b
lkowalczyk <lkowalczyk@kamsoft.pl>
2017-06-01 12:13:55 +0200

    added companion object

--- app/src/main/java/im/kowalczyk/forecastweather/data/Request.kt
+++ app/src/main/java/im/kowalczyk/forecastweather/data/Request.kt
@@ -7,12 +7,15 @@ import java.net.URL
  * Created by lkowalczyk on 01.06.2017.
  */
 
-class ForecastRequest(val url: String) {
+class ForecastRequest(val zipCode: String) {
 
-    fun run(){
-        val forecastJsonStr = URL(url).readText()
-        Log.d(javaClass.simpleName, forecastJsonStr)
+    companion object {
+        private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        private val URL = "http://api.openweathermap.org/data/2.5/" +
+                          "forecast/daily?mode=json&units=metric&cnt=7"
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
     }
+
 
 
 }
