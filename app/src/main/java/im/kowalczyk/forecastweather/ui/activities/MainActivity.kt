ac51527ca3e9f5b10e61128606d1cf0bcd3be08b
lkowalczyk <lkowalczyk@kamsoft.pl>
2017-06-01 09:48:05 +0200

    refactoring after added anko

--- app/src/main/java/im/kowalczyk/forecastweather/ui/activities/MainActivity.kt
+++ app/src/main/java/im/kowalczyk/forecastweather/ui/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import im.kowalczyk.forecastweather.R
 import im.kowalczyk.forecastweather.ui.adapters.ForecastListAdapter
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList : RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }
