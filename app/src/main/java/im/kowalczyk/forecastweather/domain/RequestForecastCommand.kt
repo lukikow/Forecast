0e74913220a059e2ec9ea65fed6ecf6905887a1c
lkowalczyk <lkowalczyk@kamsoft.pl>
2017-06-01 14:50:27 +0200

    added request command class

---
+++ app/src/main/java/im/kowalczyk/forecastweather/domain/RequestForecastCommand.kt
@@ -0,0 +1,15 @@
+package im.kowalczyk.forecastweather.domain
+
+import im.kowalczyk.forecastweather.data.ForecastRequest
+
+/**
+ * Created by lkowalczyk on 01.06.2017.
+ */
+
+class RequestForecastCommand(val zipCode: String):  Command<ForecastList> {
+    override fun execute(): ForecastList {
+        val forecastRequest = ForecastRequest(zipCode)
+        return ForecastDataMapper().convertFromDataModel(forecastRequest.execute())
+    }
+
+}
