2d008d6d3443ac8743e89034da4bd989c287191b
lkowalczyk <lkowalczyk@kamsoft.pl>
2017-06-01 08:16:36 +0200

    first commit

---
+++ app/src/main/java/im/kowalczyk/forecastweather/ForecastListAdapter.kt
@@ -0,0 +1,25 @@
+package im.kowalczyk.forecastweather
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by lkowalczyk on 31.05.2017.
+ */
+
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onBindViewHolder(p0: ViewHolder?, p1: Int) {
+        p0?.textView?.text = items[p1]
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup?, p1: Int): ViewHolder {
+        return ViewHolder(TextView(p0?.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}
