8e561136e7a18cea0143972bca230af3ed2bf19c
lkowalczyk <lkowalczyk@kamsoft.pl>
2017-06-02 08:55:49 +0200

    perform adapter to list

--- app/src/main/java/im/kowalczyk/forecastweather/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/im/kowalczyk/forecastweather/ui/adapters/ForecastListAdapter.kt
@@ -3,21 +3,25 @@ package im.kowalczyk.forecastweather.ui.adapters
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import im.kowalczyk.forecastweather.domain.ForecastList
 
 /**
  * Created by lkowalczyk on 31.05.2017.
  */
 
-class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForest: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
     override fun onBindViewHolder(p0: ViewHolder?, p1: Int) {
-        p0?.textView?.text = items[p1]
+        with(weekForest.dailyForecast[p1]) {
+            p0?.textView?.text = "$date - $description - $high/$low"
+        }
     }
+
 
     override fun onCreateViewHolder(p0: ViewGroup?, p1: Int): ViewHolder {
         return ViewHolder(TextView(p0?.context))
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForest.dailyForecast.size
 
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
