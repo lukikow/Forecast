fec16a25202ad87d1c135db7642414b3b06dbf51
lukikow <lukikow@gmail.com>
2017-07-27 12:34:05 +0200

    create Table file

---
+++ app/src/main/java/im/kowalczyk/forecastweather/data/db/Tables.kt
@@ -0,0 +1,23 @@
+package im.kowalczyk.forecastweather.data.db
+
+/**
+ * Created by lkowalczyk on 27.07.2017.
+ */
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
